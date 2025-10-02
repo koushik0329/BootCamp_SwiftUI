048dbb89e364f99ad872a5fb144e6bb89485a031
koushik0329 <kkambham@islander.tamucc.edu>
2025-10-02 10:47:39 -0500

    Initial Commit

---
+++ BootCamp_SwiftUI/BootCamp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BootCamp_SwiftUIApp.swift
+//  BootCamp_SwiftUI
+//
+//  Created by Koushik Reddy Kambham on 10/2/25.
+//
+
+import SwiftUI
+
+@main
+struct BootCamp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BootCamp_SwiftUI/TextBootCamp.swift
@@ -0,0 +1,33 @@
+//
+//  TextBootCamp.swift
+//  BootCamp_SwiftUI
+//
+//  Created by Koushik Reddy Kambham on 10/2/25.
+//
+
+
+import SwiftUI
+
+struct TextBootcamp: View {
+    var body: some View {
+        Text("Hello, I am koushik!".capitalized)
+//            .font(.body)
+//            .fontWeight(.semibold)
+//            .bold()
+//            .underline()
+//            .underline(true, color: Color.red)
+//            .italic()
+//            .strikethrough(true, color: Color.green)
+//            .font(.system(size: 24, weight: .semibold, design: .serif))
+//            .baselineOffset(-50.0)
+//            .kerning(10)
+            .multilineTextAlignment(.leading)
+            .foregroundColor(.red)
+            .frame(width: 200, height: 100, alignment: .leading)
+            .minimumScaleFactor(0.1)
+    }
+}
+
+#Preview {
+    TextBootcamp()
+}
