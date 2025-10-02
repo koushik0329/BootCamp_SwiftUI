4852ac60826bf459e03a463fdd2a0bb0e1f27b33
koushik0329 <kkambham@islander.tamucc.edu>
2025-10-02 15:05:36 -0500

    Overlay

--- BootCamp_SwiftUI/BackgroundOverlayBootCamp.swift
+++ BootCamp_SwiftUI/BackgroundOverlayBootCamp.swift
@@ -9,18 +9,34 @@ import SwiftUI
 
 struct BackgroundOverlayBootCamp: View {
     var body: some View {
-        Circle()
-            .fill(Color.pink)
-            .frame(width: 200, height: 200)
-            .overlay(
-                Text("1" )
-                    .font(.largeTitle)
-                    .foregroundStyle(.white)
+        Image(systemName: "heart.fill")
+            .font(.system(size: 40))
+            .foregroundColor(Color.white)
+            .background(
+                Circle()
+                    .fill(
+                        LinearGradient(
+                            gradient: Gradient(colors: [Color(#colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)), Color(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1))]),
+                            startPoint: .topLeading,
+                            endPoint: .bottomTrailing)
+                    )
+                    .frame(width: 100, height: 100)
+                    .shadow(color: Color(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 0.5)), radius: 10, x: 0.0, y: 10)
+                    .overlay(
+                        Circle()
+                            .fill(Color.blue)
+                            .frame(width: 35, height: 35)
+                            .overlay(
+                                Text("5")
+                                    .font(.headline)
+                                    .foregroundColor(.white)
+                            )
+                            .shadow(color: Color(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 0.5)), radius: 10, x: 5, y: 5)
+                        , alignment: .bottomTrailing)
             )
-            .background(Color.blue)
-        
     }
 }
+
 
 #Preview {
     BackgroundOverlayBootCamp()
