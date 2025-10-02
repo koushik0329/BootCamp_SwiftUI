281175f9d4e5e444ebe33b02018b8094564854b6
koushik0329 <kkambham@islander.tamucc.edu>
2025-10-02 14:55:08 -0500

    simple overlay

--- BootCamp_SwiftUI/BackgroundOverlayBootCamp.swift
+++ BootCamp_SwiftUI/BackgroundOverlayBootCamp.swift
@@ -9,21 +9,15 @@ import SwiftUI
 
 struct BackgroundOverlayBootCamp: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-            
-            .background(
-//                LinearGradient(gradient: Gradient(colors: [.red, .blue]), startPoint: .leading, endPoint: .trailing)
-//                Color.red
-                Circle()
-                    .fill(LinearGradient(gradient: Gradient(colors: [.red, .blue]), startPoint: .leading, endPoint: .trailing))
-                    .frame(width: 100, height: 100, alignment: .center)
+        Circle()
+            .fill(Color.pink)
+            .frame(width: 200, height: 200)
+            .overlay(
+                Text("1" )
+                    .font(.largeTitle)
+                    .foregroundStyle(.white)
             )
-            
-            .background(
-                Circle()
-                    .fill(LinearGradient(gradient: Gradient(colors: [.blue, .red]), startPoint: .leading, endPoint: .trailing))
-                    .frame(width: 200, height: 200, alignment: .center)
-            )
+            .background(Color.blue)
         
     }
 }
