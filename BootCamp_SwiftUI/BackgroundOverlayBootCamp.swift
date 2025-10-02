b6676705a1564ee65fde6e214116d6e904e17ff3
koushik0329 <kkambham@islander.tamucc.edu>
2025-10-02 14:34:09 -0500

    Baclground

--- BootCamp_SwiftUI/BackgroundOverlayBootCamp.swift
+++ BootCamp_SwiftUI/BackgroundOverlayBootCamp.swift
@@ -1,1 +1,34 @@
+//
+//  BackgroundOverlayBootCamp.swift
+//  BootCamp_SwiftUI
+//
+//  Created by Koushik Reddy Kambham on 10/2/25.
+//
+
+import SwiftUI
+
+struct BackgroundOverlayBootCamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            
+            .background(
+//                LinearGradient(gradient: Gradient(colors: [.red, .blue]), startPoint: .leading, endPoint: .trailing)
+//                Color.red
+                Circle()
+                    .fill(LinearGradient(gradient: Gradient(colors: [.red, .blue]), startPoint: .leading, endPoint: .trailing))
+                    .frame(width: 100, height: 100, alignment: .center)
+            )
+            
+            .background(
+                Circle()
+                    .fill(LinearGradient(gradient: Gradient(colors: [.blue, .red]), startPoint: .leading, endPoint: .trailing))
+                    .frame(width: 200, height: 200, alignment: .center)
+            )
+        
+    }
+}
+
+#Preview {
+    BackgroundOverlayBootCamp()
+}
 
