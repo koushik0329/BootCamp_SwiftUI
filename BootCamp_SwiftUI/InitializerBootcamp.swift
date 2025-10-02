ed3d7b01feeee5b875f432be43fedff2c6566ad7
koushik0329 <kkambham@islander.tamucc.edu>
2025-10-02 16:31:47 -0500

    initializer

---
+++ BootCamp_SwiftUI/InitializerBootcamp.swift
@@ -0,0 +1,52 @@
+//
+//  InitializerBootcamp.swift
+//  BootCamp_SwiftUI
+//
+//  Created by Koushik Reddy Kambham on 10/2/25.
+//
+
+import SwiftUI
+
+struct InitializerBootcamp: View {
+    
+    let backgroundColor: Color
+    let count: Int
+    let title: String
+    
+    init(count: Int, fruit: Fruit) {
+        self.count = count
+        
+        if fruit == .apple {
+            self.title = "Apples"
+            self.backgroundColor = .red
+        } else {
+            self.title = "Oranges"
+            self.backgroundColor = .orange
+        }
+    }
+    
+    enum Fruit {
+        case apple
+        case orange
+    }
+    
+    var body: some View {
+        VStack(spacing: 12) {
+            Text("\(count)")
+                .font(.largeTitle)
+                .foregroundColor(.white)
+                .underline()
+            
+            Text(title)
+                .font(.headline)
+                .foregroundColor(.white)
+        }
+        .frame(width: 150, height: 150)
+        .background(backgroundColor)
+        .cornerRadius(10)
+    }
+}
+
+#Preview {
+    InitializerBootcamp(count: 100, fruit: .apple)
+}
